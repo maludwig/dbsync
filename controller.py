773da6c49120dc985e26c442851b461a4836f25e
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2015-07-15 11:40:00 -0600

    Updated MySQL code

--- controller.py
+++ controller.py
@@ -5,18 +5,26 @@ import subprocess
 import sys
 
 def run_script(cmd):
+    print("Running: " + json.dumps(cmd))
     if type(cmd) is str:
         cmd = cmd.split(" ")
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     return out
 
-def list_dbs(user,password):
-    run_script(["./mysql.sh",user,password,"list-dbs"])
-    
+def mysql_script(func,args=[]):
+    return run_script(["./mysql.sh",rdb_user,rdb_pass,func]+args)
+
+def list_dbs():
+    return mysql_script("list-dbs")
+
+def list_tables(db):
+    return mysql_script("list-tables",[db])
+
 rdb_user = sys.argv[1]
 rdb_pass = sys.argv[2]
-print(json.dumps(list_dbs(rdb_user,rdb_pass)))  
+# print(json.dumps(run_script(["./echoer.sh",rdb_user,rdb_pass,"list-dbs"])))
+print(list_tables(sys.argv[3]))  
 # bash("./mysql.sh")
 # data = json.dumps({"list":[1,2,"three3"],"str":"boo"})
 # print(data)
--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 DBUSER=$1
 DBPASS=$2
 FUNC=$3
@@ -6,16 +7,18 @@ function list-dbs {
 	echo "show databases;" | mysql -u"${DBUSER}" -p"${DBPASS}" --skip-column-names 2> /dev/null | grep -v "information_schema" | grep -v "performance_schema" | grep -v "mysql"
 }
 function list-tables {
-	find /var/lib/mysql/${1}/ -name '*.ibd' | cut -d'/' -f2 | cut -d'.' -f1
+	find /var/lib/mysql/${1}/ -name '*.ibd' | cut -d'/' -f6 | cut -d'.' -f1
 }
 function list-updated-tables {
-	find /var/lib/mysql/${1}/ -name '*.ibd' -newer ${2} | cut -d'/' -f2 | cut -d'.' -f1
+	find /var/lib/mysql/${1}/ -name '*.ibd' -newer ${2} | cut -d'/' -f6 | cut -d'.' -f1
 }
 
 if [ "$FUNC" == "list-dbs" ]; then
 	list-dbs
 elif [ "$FUNC" == "list-tables" ]; then
-	list-tables $4
+	list-tables "$4"
+elif [ "$FUNC" == "list-updated-tables" ]; then
+	list-updated-tables "$4" "$5"
 else
 	echo 'Unrecognized command, oh no!'
 fi
