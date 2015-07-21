b4ff74a4ac5157ba6b1694d957b5024dfacc2f30
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2015-07-21 11:09:47 -0600

    Edited the controller

--- controller.py
+++ controller.py
@@ -8,23 +8,33 @@ def run_script(cmd):
     print("Running: " + json.dumps(cmd))
     if type(cmd) is str:
         cmd = cmd.split(" ")
+    cmd = ['sudo']+cmd
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     return out
+
+def sudo_script(cmd):
+    if type(cmd) is str:
+        cmd = cmd.split(" ")
+    cmd = ['sudo']+cmd
+    run_script(cmd)
 
 def mysql_script(func,args=[]):
     return run_script(["./mysql.sh",rdb_user,rdb_pass,func]+args)
 
 def list_dbs():
-    return mysql_script("list-dbs")
+    return mysql_script("list-dbs").split("\n")
 
 def list_tables(db):
-    return mysql_script("list-tables",[db])
+    return mysql_script("list-tables",[db]).split("\n")
 
-rdb_user = sys.argv[1]
-rdb_pass = sys.argv[2]
+cmd = sys.argv[1]
+rdb_user = sys.argv[2]
+rdb_pass = sys.argv[3]
 # print(json.dumps(run_script(["./echoer.sh",rdb_user,rdb_pass,"list-dbs"])))
-print(list_tables(sys.argv[3]))  
+tablist = list_tables(sys.argv[4])
+for table in tablist:
+    print("#- %s -#" % table)
 # bash("./mysql.sh")
 # data = json.dumps({"list":[1,2,"three3"],"str":"boo"})
 # print(data)
